e85c97ef937342a7a6bef948216eb0fc162e7d90
theB3NNDS <78061518+theB3NNDS@users.noreply.github.com>
2024-02-10 23:15:07 +0800

    implemented horizontal and vertical portals

--- 2D Platformer Microgame/Assets/PortalController.cs
+++ 2D Platformer Microgame/Assets/PortalController.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PortalController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform destination;
+    GameObject player;
+    Animation anim;
+    Rigidbody2D playerRb;
+
+    private void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = player.GetComponent<Animation>();
+        playerRb = player.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            if (Vector2.Distance(player.transform.position, transform.position) > 0.3f)
+                StartCoroutine(PortalIn());
+        }
     }
+
+    IEnumerator PortalIn()
+    {
+        playerRb.simulated = false;
+        anim.Play("portalIn");
+        StartCoroutine(MoveInPortal());
+        yield return new WaitForSeconds(0.5f);
+        player.transform.position = destination.transform.position;
+        playerRb.velocity = Vector2.zero;
+        anim.Play("portalOut");
+        yield return new WaitForSeconds(0.5f);
+        playerRb.simulated = true;
+    }
+
+    IEnumerator MoveInPortal()
+    {
+        float timer=0;
+        while (timer < 0.5f){
+            player.transform.position = Vector2.MoveTowards(player.transform.position, transform.position, 3 * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+        }
+    }
+
+
 }
